4322eba584b3038d8512f5a07ed684f32fec2ea2
Lucas Sifoni <lucas@documents.design>
2021-10-12 17:04:10 +0200

    Ooops. Rolled keys.

--- release.sh
+++ release.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source .env.prod
 
-export SECRET_KEY_BASE=oHtWgTihTvk17EAMCpmyvsymnpL9YBdsM+E/9IzJrcT7hZQW/4mGkO+ern0hmL8o
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 MIX_ENV=prod mix assets.deploy
